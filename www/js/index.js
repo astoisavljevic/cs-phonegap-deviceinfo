6f0ed23220477ba8959d33a7946be28588fd7749
Aleksandar Stoisavljevic <staleks@gmail.com>
2013-10-21 22:58:37 +0200

    [add:]

---
+++ www/js/index.js
@@ -0,0 +1,25 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+
+        // find template section block
+        deviceNameTemplateText = $('#deviceName').html();
+        // convert it to function
+        deviceNameTemplate = _.template(deviceNameTemplateText);
+        // parse deviceName
+        var deviceName1 = { deviceNamePlaceholder : 'MB525'};
+        parsedTemplate = deviceNameTemplate(deviceName1);
+
+        $('#deviceready').html(parsedTemplate);
+    }
+};
