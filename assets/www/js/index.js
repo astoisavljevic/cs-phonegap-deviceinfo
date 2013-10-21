f9c11dfe40ff39dc09e23361f2b63d881a18f743
Aleksandar Stoisavljevic <staleks@gmail.com>
2013-10-21 15:54:52 +0200

    [update:] reorganized code to be pretty

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -14,36 +14,63 @@ var app = {
     //
     onDeviceReady: function() {        
         var element = document.getElementById('deviceProperties');
+
+        var deviceName = 
+        	'<div class=\"device-property-item\">' + 
+        		'<div class=\"device-property-item-left\">' + 
+        			'<span>Device name:</span>' +
+        		'</div>' +
+        		'<div class=\"device-property-item-right\">' + 
+        			'<span>' + device.name +'</span>' +
+        		'</div>' +
+        		'<div style=\"clear: both;\"></div>' +
+        	'</div>';
         
-        element.innerHTML = '<div class=\"device-property-item\">' +
-			'<div class=\"device-property-item-left\">' +
-			'<span>Device name:</span></div>' + 
-			'<div class=\"device-property-item-right\">' + 
-			'<span>' + device.name +'</span></div>' +
-			'<div style=\"clear: both;\"></div></div>' +
-        	'<div class=\"device-property-item\">' +
-			'<div class=\"device-property-item-left\">' +
-			'<span>PhoneGap:</span></div>' + 
-			'<div class=\"device-property-item-right\">' + 
-			'<span>' + device.phonegap +'</span></div>' +
-			'<div style=\"clear: both;\"></div></div>' +
-        	'<div class=\"device-property-item\">' +
-			'<div class=\"device-property-item-left\">' +
-			'<span>Device Platform:</span></div>' + 
-			'<div class=\"device-property-item-right\">' + 
-			'<span>' + device.platform +'</span></div>' +
-			'<div style=\"clear: both;\"></div></div>' +
-        	'<div class=\"device-property-item\">' +
-			'<div class=\"device-property-item-left\">' +
-			'<span>Device UUID:</span></div>' + 
-			'<div class=\"device-property-item-right\">' + 
-			'<span>' + device.uuid +'</span></div>' +
-			'<div style=\"clear: both;\"></div></div>' +
-        	'<div class=\"device-property-item\">' +
-			'<div class=\"device-property-item-left\">' +
-			'<span>Device Version::</span></div>' + 
-			'<div class=\"device-property-item-right\">' + 
-			'<span>' + device.version +'</span></div>' +
-			'<div style=\"clear: both;\"></div></div>';
+        var phoneGapVersion = 
+        	'<div class=\"device-property-item\">' + 
+        		'<div class=\"device-property-item-left\">' + 
+        			'<span>PhoneGap:</span>' +
+        		'</div>' +
+        		'<div class=\"device-property-item-right\">' + 
+        			'<span>' + device.phonegap +'</span>' +
+        		'</div>' +
+        		'<div style=\"clear: both;\"></div>' +
+        	'</div>';
+        	
+        var devicePlatform = 
+        	'<div class=\"device-property-item\">' + 
+        		'<div class=\"device-property-item-left\">' + 
+        			'<span>Device Platform:</span>' +
+        		'</div>' +
+        		'<div class=\"device-property-item-right\">' + 
+        			'<span>' + device.platform +'</span>' +
+        		'</div>' +
+        		'<div style=\"clear: both;\"></div>' +
+        	'</div>';
+        
+        var deviceUUID = 
+        	'<div class=\"device-property-item\">' + 
+        		'<div class=\"device-property-item-left\">' + 
+        			'<span>Device UUID:</span>' +
+        		'</div>' +
+        		'<div class=\"device-property-item-right\">' + 
+        			'<span>' + device.uuid +'</span>' +
+        		'</div>' +
+        		'<div style=\"clear: both;\"></div>' +
+        	'</div>';
+        	
+        var deviceVersion = 
+        	'<div class=\"device-property-item\">' + 
+        		'<div class=\"device-property-item-left\">' + 
+        			'<span>Device UUID:</span>' +
+        		'</div>' +
+        		'<div class=\"device-property-item-right\">' + 
+        			'<span>' + device.version +'</span>' +
+        		'</div>' +
+        		'<div style=\"clear: both;\"></div>' +
+        	'</div>';
+        
+        element.innerHTML = deviceName + phoneGapVersion + devicePlatform + deviceUUID + 
+        	deviceVersion;
     }
 };
