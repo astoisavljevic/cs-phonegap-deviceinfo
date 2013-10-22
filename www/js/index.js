c84c8d277a62bb1a870b16c4f0a12e67b3727ef5
Aleksandar Stoisavljevic <staleks@gmail.com>
2013-10-22 17:08:46 +0200

    [update:] using underscore.js

--- www/js/index.js
+++ www/js/index.js
@@ -10,22 +10,17 @@ var app = {
         app.receivedEvent('deviceready');
     },
     receivedEvent: function(id) {
-        console.log('Received Event: ' + id);
-
         // find template section block
         templateText = $('#template').html();
-        // convert it to function
-        templateFunction = _.template(templateText);
-        // parse deviceName
-        var deviceProperties = [
-            {key: 'Device Name', value: device.name},
-            {key: 'PhoneGap', value: device.phonegap},
-            {key: 'Device Platform', value: device.platform},
-            {key: 'Device UUID', value: device.uuid},
-            {key: 'OS Version', value: device.version}
-        ];
-        parsedTemplate = templateFunction(deviceProperties);
 
-        $('#deviceProperties').html(parsedTemplate);
+        var deviceProperties = [
+            {key: "Device Name", value: device.name},
+            {key: "PhoneGap", value: device.phonegap},
+            {key: "Device Platform", value: device.platform},
+            {key: "Device UUID", value: device.uuid},
+            {key: "OS Version", value: device.version}
+        ];
+
+        $('#deviceProperties').html(_.template(templateText, {deviceProperties: deviceProperties}));
     }
 };
